5b643075bd8f9a1efbeddcec21dc0706da83f091
Miracle Okeke <mokeke185@gmail.com>
2023-10-01 17:52:27 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -5,62 +5,41 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
 const nodemailer = require("nodemailer");
-const { createFFmpeg, fetchFile } = require("@ffmpeg/ffmpeg");
+const ffmpeg = require("fluent-ffmpeg"); // Use fluent-ffmpeg for Node.js
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 
-// Set up FFmpeg
-const ffmpeg = createFFmpeg({ log: true });
+// Set up multer for handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-async function initFFmpeg() {
-  await ffmpeg.load();
-}
+// Parse JSON and form data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-initFFmpeg()
-  .then(() => {
-    // Set up multer for handling file uploads
-    const storage = multer.memoryStorage();
-    const upload = multer({ storage: storage });
+// Define routes for other parts of your application...
 
-    // Parse JSON and form data
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+// Handle video uploads and conversion to MP4
+app.post("/api/upload", upload.single("video"), async (req, res) => {
+  try {
+    const { buffer } = req.file;
 
-    // Define routes for other parts of your application...
+    // Save the uploaded file to a temporary location
+    const tempFilePath = "temp.webm";
+    fs.writeFileSync(tempFilePath, buffer);
 
-    // Handle video uploads and conversion to MP4
-    app.post("/api/upload", upload.single("video"), async (req, res) => {
-      try {
-        const { buffer } = req.file;
-
-        // Save the uploaded file to a temporary location
-        const tempFilePath = "temp.webm";
-        fs.writeFileSync(tempFilePath, buffer);
-
-        // Convert the video to MP4 using FFmpeg
-        await ffmpeg.FS(
-          "writeFile",
-          "input.webm",
-          await fetchFile(tempFilePath)
-        );
-        await ffmpeg.run(
-          "-i",
-          "input.webm",
-          "-c:v",
-          "libx264",
-          "-an",
-          "output.mp4"
-        );
-        const mp4Data = await ffmpeg.FS("readFile", "output.mp4");
-
+    // Convert the video to MP4 using fluent-ffmpeg
+    ffmpeg(tempFilePath)
+      .outputOptions("-c:v libx264 -an")
+      .on("end", () => {
         // Generate a unique filename
         const timestamp = Date.now();
         const mp4Filename = `${timestamp}.mp4`;
 
-        // Save the MP4 file
-        fs.writeFileSync(mp4Filename, mp4Data);
+        // Move the MP4 file to the desired location
+        fs.renameSync(tempFilePath + ".mp4", mp4Filename);
 
         // Respond with the URL of the MP4 file
         const mp4Url = `https://chrome-fd0g.onrender.com/${mp4Filename}`;
@@ -68,21 +47,21 @@ initFFmpeg()
 
         // Clean up temporary files
         fs.unlinkSync(tempFilePath);
-        fs.unlinkSync("input.webm");
-        fs.unlinkSync("output.mp4");
-      } catch (error) {
-        console.error("Error processing video:", error);
+      })
+      .on("error", (err) => {
+        console.error("Error processing video:", err);
         res.status(500).json({ error: "Error processing video" });
-      }
-    });
+      })
+      .save(tempFilePath + ".mp4");
+  } catch (error) {
+    console.error("Error processing video:", error);
+    res.status(500).json({ error: "Error processing video" });
+  }
+});
 
-    // Define routes for other parts of your application...
+// Define routes for other parts of your application...
 
-    // Start the server
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error initializing FFmpeg:", error);
-  });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
